35991efb807dd6a9aa7075159f7fbf21cfcd7f96
David Neill-Asanza <davidhneill@gmail.com>
2020-10-02 15:47:33 +0200

    Finished exercise 3.

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -2,14 +2,15 @@
 """implement a polynomial basis function."""
 
 import numpy as np
-
-
+    
 def build_poly(x, degree):
     """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+    if (degree < 0): raise ValueError("degree must be positive")
+    
+    phi = np.zeros((len(x), degree+1))
+    
+    phi[:,0] = 1
+    for d in range(1,degree+1):
+        phi[:,d] = phi[:,d-1] * x
+
+    return phi
